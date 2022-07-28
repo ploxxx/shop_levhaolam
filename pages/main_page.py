82ea438b339c8675cc85218a9f2c7d2227492969
Vlad Zharkov <zharkov.kill@yandex.by>
2022-07-28 11:37:22 +0300

    correction file

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.options import Options
 from .base_page import BasePage
 from .locators import MainPageLocators
 
+
 class MainPage(BasePage):
    
     def go_to_sign_in(self):
